1c56b923d5a30193297062fadda998b7473c8b9a
Débora Rodrigues <paulohenrique8190@gmail.com>
2021-09-05 15:36:10 -0300

    adicionando sessão de filmes na tela de home Netflix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import logoODiaboDeCadaDia from "../../assets/o-diabo-de-cada-dia-logo.png";
+import djangoImg from "../../assets/django-livre.jpg";
 import "./styles.css";
 
 
@@ -34,13 +36,43 @@ export default function Home() {
                 </header>
             </div>
             <div id="hero" className="container-fluid">
-                <div className="container">
+                <div className="container-fluid">
                     <div className="row" id="hero_infos">
                         <div className="col-6">
-                            <img className="logo" src="" alt="" />
+                            <img className="logo" src={logoODiaboDeCadaDia} />
+                            <h1 className="text-white">Top 1 de hoje no Brasil.</h1>
+                            <p className="text-white">
+                                Um jovem dedicado a proteger seus entes queridos 
+                                enfrenta a corrupção e a violência em um canto esquecido de Ohio, 
+                                no período entre a Segunda Guerra Mundial e a Guerra do Vietnã.
+                            </p>
+                            <button className="btn btn-lg btn-custom-white">
+                                <span className="mdi mdi-play"></span> Assistir
+                            </button>
+                            <button className="btn btn-lg btn-custom-trasnparent">
+                                <span className="mdi mdi-information-outline"></span> Mais informações
+                            </button>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="main-content">
+                <section>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12">
+                                <h5 className="text-white">Populares da Netflix</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <ul className="filme_lista">
+                            <li className="filme">
+                                <img className="img-fluid" src={djangoImg}/>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
             </div>
         </>
     );
