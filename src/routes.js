208d77ebbddc0533dcc377df82baa27b76c2bbc9
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-10-02 15:20:14 -0300

    Compatibilidade com o FireFox em algumas estilizaçoes

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login/Login.jsx";
 import Home from "./pages/Home/Home.jsx";
+import Register from "./pages/Register/Register.jsx";
 
 export default function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Login} />
-                <Route path="/home" exact component={Home} />
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/home" exact component={Home} />
+        <Route path="/register" exact component={Register}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
